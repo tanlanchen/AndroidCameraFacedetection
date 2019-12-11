ad72c659533c14193a91f7465ed4951bd78286aa
tanlanchen <46107353+tanlanchen@users.noreply.github.com>
2019-12-11 20:04:28 +0800

    Update Face.java

--- app/src/main/java/com/example/httpdemo/Face.java
+++ app/src/main/java/com/example/httpdemo/Face.java
@@ -2,10 +2,7 @@ package com.example.httpdemo;
 
 import org.opencv.core.Rect;
 
-/**
- * Stay Hungry Stay Foolish
- * Author: dp on 2019/3/25 12:51
- */
+
 public class Face {
     public Rect faceRect;
     public int faceConfidence;
